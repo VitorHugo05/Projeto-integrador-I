b11aaf3b2af7a3a5f462af13bcf447a3350c9b2f
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2025-02-23 16:15:48 -0300

    🚀 feat: Project created

---
+++ backend/event/src/test/java/com/einstein/event/EventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einstein.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
